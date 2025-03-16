f0cc3ad8d9e12f8d7a97f97a113ab92a1f9e56ad
ghufron akbar <alan.040219.gam@gmail.com>
2025-03-16 15:09:22 +0700

    add debugging

--- controllers/user/call_controller.js
+++ controllers/user/call_controller.js
@@ -17,10 +17,10 @@ exports.makecall = function (req, res) {
   let longitude = req.body.longitude
   let status = 0
   let type = req.body.type
+  console.log({ message, latitude, longitude, datetimenow, status, type })
   let now = new Date()
   let datetimenow = now.getFullYear() + '-' + ('0' + (now.getMonth() + 1)).slice(-2) + '-' + ('0' + now.getDate()).slice(-2) + ' ' +
     ('0' + now.getHours()).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2) + ':' + ('0' + now.getSeconds()).slice(-2);
-
   let id_user = req.decoded.id_user
   console.log(id_user)
 
